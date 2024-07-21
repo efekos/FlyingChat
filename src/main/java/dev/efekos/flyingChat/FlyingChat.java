01af2a3c5513747215e1100b118235c329201292
efekos <56752400+efekos@users.noreply.github.com>
2024-07-21 22:00:48 +0300

    Initial commit

---
+++ src/main/java/dev/efekos/flyingChat/FlyingChat.java
@@ -0,0 +1,17 @@
+package dev.efekos.flyingChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlyingChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
