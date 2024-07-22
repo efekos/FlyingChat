1607478a736dab32360495e4e5286a17a5ca3be4
efekos <56752400+efekos@users.noreply.github.com>
2024-07-22 21:51:13 +0300

    add colorful text support

--- src/main/java/dev/efekos/flyingChat/FlyingChat.java
+++ src/main/java/dev/efekos/flyingChat/FlyingChat.java
@@ -1,9 +1,11 @@
 package dev.efekos.flyingChat;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.World;
 import org.bukkit.entity.Display;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.TextDisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,6 +14,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Transformation;
+import org.checkerframework.checker.units.qual.C;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
@@ -30,25 +33,53 @@ public final class FlyingChat extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChat(AsyncPlayerChatEvent e) {
+        e.setCancelled(true);
        new BukkitRunnable(){
 
            private final FlyingChat instance = FlyingChat.this;
 
            @Override
            public void run() {
-               World world = e.getPlayer().getWorld();
-               TextDisplay entity = (TextDisplay) world.spawnEntity(e.getPlayer().getLocation().add(0,2,0), EntityType.TEXT_DISPLAY);
+               Player p = e.getPlayer();
+               World world = p.getWorld();
+               TextDisplay entity = (TextDisplay) world.spawnEntity(p.getLocation().add(0,2,0), EntityType.TEXT_DISPLAY);
                entity.setAlignment(TextDisplay.TextAlignment.CENTER);
                entity.setBillboard(Display.Billboard.CENTER);
-               entity.setText(e.getMessage());
+               entity.setText(findColor(p)+e.getMessage());
                entity.setShadowed(true);
                entity.setBackgroundColor(Color.fromARGB(0,0,0,0));
                entity.setTransformation(new Transformation(new Vector3f(),new Quaternionf(),new Vector3f(1.5f,1.5f,1.5f),new Quaternionf()));
                new EntityFlyRunnable(entity).runTaskTimer(instance,1,1);
-
-               e.setCancelled(true);
            }
        }.runTaskLater(this,5);
+    }
+
+    private static final ChatColor[] ALL_COLORS = new ChatColor[]{
+            ChatColor.YELLOW,
+            ChatColor.GOLD,
+            ChatColor.AQUA,
+            ChatColor.RED,
+            ChatColor.BLUE,
+            ChatColor.DARK_AQUA,
+            ChatColor.DARK_BLUE,
+            ChatColor.DARK_GRAY,
+            ChatColor.DARK_GREEN,
+            ChatColor.DARK_PURPLE,
+            ChatColor.DARK_RED,
+            ChatColor.GREEN,
+            ChatColor.BLACK,
+            ChatColor.LIGHT_PURPLE
+    };
+
+    private Color findColor(Player player) {
+        ChatColor color = findTextColor(player);
+        java.awt.Color c = color.asBungee().getColor();
+        return Color.fromRGB(c.getRed(),c.getGreen(),c.getBlue());
+    }
+
+    private ChatColor findTextColor(Player player){
+        for (ChatColor color : ALL_COLORS) if(player.hasPermission("flyingchat."+color.name())) return color;
+        return ChatColor.WHITE;
     }
 
 }
