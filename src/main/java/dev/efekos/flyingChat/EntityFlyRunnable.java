1be1abbf1fbd53a1bfca185579dd7792688913d8
efekos <56752400+efekos@users.noreply.github.com>
2024-07-22 22:35:54 +0300

    reformat code

--- src/main/java/dev/efekos/flyingChat/EntityFlyRunnable.java
+++ src/main/java/dev/efekos/flyingChat/EntityFlyRunnable.java
@@ -25,28 +25,26 @@
 
 package dev.efekos.flyingChat;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.TextDisplay;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class EntityFlyRunnable extends BukkitRunnable {
 
     private final TextDisplay entity;
+    private int ticks = 0;
 
     public EntityFlyRunnable(TextDisplay entity) {
         this.entity = entity;
     }
 
-    private int ticks = 0;
-
     @Override
     public void run() {
-        if(++ticks>60){
+        if (++ticks > 60) {
             entity.remove();
             cancel();
             return;
         }
-        entity.setTextOpacity((byte) (entity.getTextOpacity()-4));
-        entity.teleport(entity.getLocation().add(0,.1,0));
+        entity.setTextOpacity((byte) (entity.getTextOpacity() - 4));
+        entity.teleport(entity.getLocation().add(0, .1, 0));
     }
 }
